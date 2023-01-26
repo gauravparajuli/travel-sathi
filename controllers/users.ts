3d53237e67e49ffb55ea40a59e5ea6c16e627d62
gaurav parajuli <gparajuli.1997@gmail.com>
2023-01-26 15:14:28 +0545

    implemented login in user controller

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 import User from '../models/User'
 import { CError } from '../types/CError'
@@ -29,7 +30,7 @@ export const newUser: RequestHandler = async (
             throw err
         }
 
-        const hashedPassword = bcrypt.hashSync(password)
+        const hashedPassword = bcrypt.hashSync(password, 7)
 
         const newUser = new User({ email, password: hashedPassword })
         await newUser.save()
@@ -45,10 +46,52 @@ export const loginUser: RequestHandler = async (
     res: Response,
     next: NextFunction
 ) => {
-    // try {
-    //     const records = await Category.find({ category: 'expense' })
-    //     res.status(200).json(records)
-    // } catch (error) {
-    //     next(error)
-    // }
+    try {
+        const { error } = validateUser(req.body)
+        if (error) {
+            const err = new CError('input validation failed')
+            err.statusCode = 422
+            err.details = error.details
+            throw err
+        }
+
+        const { email, password } = req.body
+
+        // verify email and password
+
+        const userInstance = await User.findOne({ email })
+        if (!userInstance) {
+            const error = new CError('invalid user or password.')
+            error.statusCode = 401
+            throw error
+        }
+
+        // valid user now check for password
+        const isPasswordMatching = await bcrypt.compare(
+            password,
+            userInstance.password
+        )
+        if (!isPasswordMatching) {
+            const error = new CError('invalid user or password.')
+            error.statusCode = 401
+            throw error
+        }
+
+        const accessToken = jwt.sign(
+            {
+                id: userInstance._id,
+                isAdmin: userInstance.isManager,
+            },
+            process.env.JWT_SECRET_KEY || 'jwtsecretkey',
+            { expiresIn: process.env.JWT_TOKEN_LIFE || '3d' }
+        )
+
+        res.status(200).json({
+            _id: userInstance._id,
+            email: userInstance.email,
+            accessToken,
+        })
+    } catch (error) {
+        next(error)
+    }
 }
